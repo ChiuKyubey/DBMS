714372216fca30a593b9e030c5114e16069ccac8
ChiuKyubey <ndhuong1616@gmail.com>
2024-04-28 10:39:38 +0700

    them xoa sua TG_SoLuongNhanVien CaLam

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,58 +1,80 @@
-﻿USE QLNhaSach
+﻿-- Quản lý nhân viên 
+USE QLNhaSach
 GO
--- Proc_XemThongTinCaNhan
-CREATE PROCEDURE Proc_XemThongTinCaNhan
+CREATE PROCEDURE XemThongTinNhanVien
 AS
 BEGIN
-    DECLARE @TenTaiKhoan VARCHAR(50);
-    SET @TenTaiKhoan = ORIGINAL_LOGIN()
-
-    IF EXISTS (SELECT 1 FROM NhanVien WHERE TenTaiKhoan = @TenTaiKhoan)
-    BEGIN
-        SELECT MaNV, TenNV, GioiTinh, DiaChi, TenTaiKhoan, Cap, ChucVu
-        FROM NhanVien
-        WHERE TenTaiKhoan = @TenTaiKhoan;
-    END
-    ELSE
-    BEGIN
-        RAISERROR ('Tài khoản không tồn tại!', 16, 1);
-    END
+    SELECT 
+        MaNV,
+        TenNV,
+        GioiTinh,
+        DiaChi,
+        Cap,
+        ChucVu,
+        Anh
+    FROM 
+        NhanVien;
 END;
+GO
+--View_CaLam
+--WITH SortedCaLam AS (
+--    SELECT MaNV, Ca, Thu,
+--           CASE 
+--               WHEN Thu = 'Monday' THEN 1
+--               WHEN Thu = 'Tuesday' THEN 2
+--               WHEN Thu = 'Wednesday' THEN 3
+--               WHEN Thu = 'Thursday' THEN 4
+--               WHEN Thu = 'Friday' THEN 5
+--               WHEN Thu = 'Saturday' THEN 6
+--               WHEN Thu = 'Sunday' THEN 7
+--           END AS ThuOrder
+--    FROM CaLamViec
+--)
+--SELECT MaNV, Ca, Thu
+--FROM SortedCaLam
+--ORDER BY ThuOrder;
 
+--Proc_ThemCaLam
 GO
-EXEC Proc_XemThongTinCaNhan
-SELECT * FROM NhanVien WHERE TenTaiKhoan = ORIGINAL_LOGIN();
-SELECT SESSION_USER;
-SELECT ORIGINAL_LOGIN();
--- Proc_SuaThongTinCaNhan
-GO
-CREATE PROCEDURE Proc_SuaThongTinCaNhan
-    @TenNV NVARCHAR(50),
-    @GioiTinh NVARCHAR(10),
-    @DiaChi NVARCHAR(255),
-    @MatKhauMoi VARCHAR(50),
-    @CapMoi INT,
-    @ChucVu NVARCHAR(50) = '',
-    @Anh IMAGE = NULL
+CREATE PROCEDURE Proc_ThemCaLam
+    @MaNV INT,
+    @Ca VARCHAR(50),
+    @Thu VARCHAR(50)
 AS
 BEGIN
-    DECLARE @TenTaiKhoan VARCHAR(50);
-    SET @TenTaiKhoan = CURRENT_USER;
-
-    IF EXISTS (SELECT 1 FROM NhanVien WHERE TenTaiKhoan = @TenTaiKhoan)
-    BEGIN
-        UPDATE NhanVien
-        SET TenNV = @TenNV,
-            GioiTinh = @GioiTinh,
-            DiaChi = @DiaChi,
-            MatKhau = @MatKhauMoi,
-            Cap = @CapMoi,
-            ChucVu = @ChucVu,
-            Anh = @Anh
-        WHERE TenTaiKhoan = @TenTaiKhoan;
-    END
-    ELSE
-    BEGIN
-        RAISERROR ('Tài khoản không tồn tại!', 16, 1);
-    END
+    INSERT INTO CaLamViec (MaNV, Ca, Thu)
+    VALUES (@MaNV, @Ca, @Thu);
 END;
+
+--Proc_XoaCaLam
+GO
+CREATE PROCEDURE Proc_XoaCaLam
+    @MaNV INT
+AS
+BEGIN
+    DELETE FROM CaLamViec WHERE MaNV = @MaNV;
+END;
+--Proc_SuaCaLam
+GO
+CREATE PROCEDURE Proc_SuaCaLam
+    @MaNV INT,
+    @Ca VARCHAR(50),
+    @Thu VARCHAR(50)
+AS
+BEGIN
+    UPDATE CaLamViec
+    SET Ca = @Ca, Thu = @Thu
+    WHERE MaNV = @MaNV;
+END;
+--TG_KiemTraSoLuong (3 ca, moi ca co ThuNgan: 2 nguoi, QuanLyKho: 2 nguoi)
+GO
+CREATE FUNCTION TG_KiemTraSoLuong (@Ca VARCHAR(50))
+RETURNS INT
+AS
+BEGIN
+    DECLARE @SoLuong INT;
+    SET @SoLuong = (SELECT COUNT(*) FROM CaLamViec WHERE Ca = @Ca);
+    RETURN @SoLuong;
+END;
+--Func_TimKiemNhanVien
+
